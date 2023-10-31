bd8c433fce8fd945a886595b3fd804f8e5f4816c
Catalin Boja <catalin.boja@ie.ase.ro>
2023-10-31 21:22:57 +0200

    Extra meeting end

--- Extras/Extra_02/Extra.cpp
+++ Extras/Extra_02/Extra.cpp
@@ -8,11 +8,11 @@ using namespace std;
 //constructor/functions overloading
 //long cout vs printf
 //destructor
-// copy ctor 
+//copy ctor 
 //enum class vs enum
 //shallow copy
 
-
+enum class TestType {BACHELOR, MASTER};
 
 class StudentGroup {
 	int groupNo = 1071; //code C1056 - Cybernetics 1056
@@ -91,7 +91,79 @@ public:
 	StudentGroup() {
 
 	}
+
+	void setStudents(string* students, int  noStudents) {
+		delete[] this->students;
+		this->students = new string[noStudents];
+		for (int i = 0; i < noStudents; i++) {
+			this->students[i] = students[i];
+		}
+		this->noStudents = noStudents;
+	}
+
+	void printStudentsList() {
+		if (this->students != nullptr) {
+			cout << endl << "Students: ";
+			for (int i = 0; i < this->noStudents; i++) {
+				cout << " " << this->students[i];
+			}
+		}
+		else {
+			cout << endl << "No students";
+		}
+	}
+
+	string* getStudentsList() {
+		//don't do this
+		//return this->students;
+
+		string* copy = new string[this->noStudents];
+		for (int i = 0; i < this->noStudents; i++)
+		{
+			copy[i] = this->students[i];
+		}
+
+		return copy;
+	}
+
+	~StudentGroup() {
+		cout << endl << "Apel destructor";
+		delete[] this->students;
+	}
+
+	StudentGroup(StudentGroup& group) {
+		cout << endl << "Calling copy ctor";
+		this->code = group.code;
+		//this->students = group.students;
+		this->students = group.getStudentsList();
+		this->noStudents = group.noStudents;
+	}
 };
+
+void takeExam() {
+	StudentGroup g1071;
+	string students[] = { "John", "Alice", "Bob" };
+	g1071.setStudents(students, 3);
+}
+
+void takeExam(StudentGroup group) {
+	cout << endl << group.getNumber() << " takes the exam";
+}
+
+void takeExam(StudentGroup* group) {
+	cout << endl << group->getNumber() << " takes the exam";
+}
+
+StudentGroup doSomething() {
+	StudentGroup g1071;
+	string students[] = { "John", "Alice", "Bob" };
+	g1071.setStudents(students, 3);
+	return g1071;
+}
+
+//void takeExam(StudentGroup& group) {
+//	cout << endl << group.getNumber() << " takes the exam";
+//}
 
 int main() {
 	StudentGroup g1071;
@@ -108,4 +180,38 @@ int main() {
 	StudentGroup g1500('M', 1500);
 	StudentGroup g1768(1768, false);
 
+	string students[] = { "John", "Alice", "Bob" };
+	g1072.setStudents(students, 3);
+
+	g1072.printStudentsList();
+
+	string* studentsList = g1072.getStudentsList();
+	studentsList[0] = "Vader";
+	delete[] studentsList;
+
+	g1072.printStudentsList();
+
+	//for (;;) {
+	//	takeExam();
+	//}
+
+	takeExam();
+
+	StudentGroup* pGrupa = new StudentGroup('C', 1071);
+	pGrupa->setStudents(students, 3);
+	delete pGrupa;
+
+	takeExam(g1072);
+
+	//calling copy ctor
+	StudentGroup g1072Clone = g1072;
+
+	TestType type = TestType::BACHELOR;
+	//works with simple enum
+	//if (type == 0) {
+
+	if (type == TestType::BACHELOR) {
+		cout << endl << "Is Bachelor";
+	}
+	
 }
